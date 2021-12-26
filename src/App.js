b082fae3377b1776d4f7beb51c329e7ec0da0d94
itai rozen <itai.rozen@mail.huji.ac.il>
2021-12-26 06:50:48 +0200

    homepage working but lacks design

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default class App extends React.Component {
     }
   }
 
+
   updateError = msg => this.setState({error: msg})
   componentDidMount =  () => {
      this.updateState()
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,78 +1,94 @@
 import React from 'react'
-import _ from 'lodash'
-import Spinner from '../../components/Spinner/Spinner'
 import Question from '../../components/Question/Question'
 import Answer from '../../components/Answer/Answer'
-import './homepage.css'
 import Button from '../../components/Button/Button'
+import './homepage.css'
 
 export default class Homepage extends React.Component {
 
     state = {
         error: this.props.error,
-        flashCards: [],
         lastPlayed: '',
-        roundCards: [],
-        currCard: { question: '', answer: '' },
-        isFlipped: false
+        isFlipped: false,
+        isResolved: true,
+        isDone: false,
+        cards: this.props.flashCards,
+        roundCards: []
     }
 
     popCard = () => {
         const roundCardsCopy = [...this.state.roundCards]
         const currCard = roundCardsCopy.pop()
-        console.log('current card: ', currCard)
-        console.log('round cards:', this.state.roundCards)
-        this.setState({ currCard: currCard, roundCards: roundCardsCopy, isFlipped: false })
-        console.log('currentShown: ', this.state.currCard)
-        console.log('copy:', roundCardsCopy)
-        console.log('original: ', this.state.roundCards)
+        this.setState({ currCard: currCard, roundCards: roundCardsCopy, isFlipped: false, isResolved:!this.state.isResolved })
+        console.log(this.state.roundCards)
+
     }
 
     flipCard = () => {
         this.setState({ isFlipped: true })
+        if (this.state.roundCards.length === 0){
+            this.setState({isDone:true})
+            return
+        }
     }
 
 
-
+    backToThePile = () => {
+        const roundCardsCopy = [...this.state.roundCards]
+        roundCardsCopy.unshift(this.state.currCard)
+        this.setState({ roundCards: roundCardsCopy , isFlipped: false, isResolved:!this.state.isResolved})
+    }
+    startRound = () => {
+        const shuffled = [...this.state.cards]
+        this.setState({isDone:false, roundCards: shuffled.sort(() => 0.5 - Math.random()) })
+    }
     componentDidMount = () => {
-        const cardsCopy = [...this.props.flashCards]
-        const shuffleCards = cardsCopy.sort(() => 0.5 - Math.random())
-        console.log('copy: ', cardsCopy)
-        console.log('shuffle: ', shuffleCards)
-        this.setState(() => { return { roundCards: shuffleCards } })
-        this.popCard()
+        this.startRound()
     }
-
-
-
-
 
 
     render() {
 
         return <div className='homepage-container'>
+            <div className={`flip-card ${this.state.isFlipped && 'flipped'}`}>
 
-            <div className={`flip-card ${this.state.isFlipped && "flipped"}`}>
-            </div>
-            <div className="flip-card-inner" >
-                <div className="flip-card-front">
-                    {this.state.currCard &&
-                        <Question question={this.state.currCard.question} />
-                    }
+                <div className={`flip-card-inner }`} >
+                    <div className="flip-card-front">
+                        {this.state.currCard &&
+                            <Question question={this.state.currCard.question} />
+                        }
+                    </div>
+                    <div className="flip-card-back">
+                        {this.state.currCard &&
+                            <Answer answer={this.state.currCard.answer} />
+                        }
+                    </div>
                 </div>
-                <div className="flip-card-back">
-                    {this.state.currCard &&
-                        <Answer answer={this.state.currCard.answer} />
-                    }
+            </div>
+            <div className="progression">{this.state.cards.length-this.state.roundCards.length}/{this.state.cards.length}</div>
+            <button disabled={!this.state.isResolved || this.state.isDone} onClick={this.popCard}>Reveal Card</button>
+            <button disabled={this.state.isResolved || this.state.isDone}  onClick={this.flipCard}>Reveal answer</button>
+            <div className="resolve-container">
+
+            {(this.state.isFlipped && !this.state.isDone ) &&
+                <div>
+                    <h3>Did you get it right?</h3>
+                    <button onClick={() => this.setState({isFlipped: false, isResolved:!this.state.isResolved})}>
+                        yes
+                    </button>
+                    <button onClick={this.backToThePile}>
+                        no
+                    </button>
                 </div>
+            }
             </div>
-            <button onClick={() => this.popCard()}>reveal card</button>
-            {/* <Button content="Reveal Card" clickFunction={this.popCard} /> */}
-            <Button content="Reveal anwer" clickFunction={this.flipCard} />
-            <div>
-                <h3>Did you get it right?</h3>
-                <Button content="yes" clickFunction={this.popCard} />
+            <div className="done-conatainer">
+            {this.state.isDone && <div>
+                    <h2>you're done!</h2>    
+                    <button onClick={this.startRound}>start over?</button>
+            </div>}
             </div>
+
         </div>
 
     }
